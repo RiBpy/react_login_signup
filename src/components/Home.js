115e216e5ce6c1e981e380513508d6f86bf439f9
ribpy <mdriazbappy22@gmail.com>
2022-10-22 19:53:35 +0600

    some imges change

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,13 +9,13 @@ const Home = () => {
           <div className='py-8 px-4'>
             <div className='grid grid-cols-2 md:grid-cols-4 gap-6  '>
               <img src='./assets/bulbBlack.jpg' alt='img-demo' className='w-[100%] h-60 rounded-md '/>
-              <img src='./assets/cat.jpg' alt='img-demo' className='w-[100%] h-60 rounded-md '/>
+              <img src='./assets/pasta.jpg' alt='img-demo' className='w-[100%] h-60 rounded-md '/>
               <img src='./assets/table3.jpg' alt='img-demo' className='w-[100%] h-60 rounded-md '/>
               <img src='./assets/table6.jpg' alt='img-demo' className='w-[100%] h-60 rounded-md '/>
-              <img src='./assets/flo.jpg' alt='img-demo' className='w-[100%] h-[90%] rounded-md '/>
+              <img src='./assets/cofee.jpg' alt='img-demo' className='w-[100%] h-[90%] rounded-md '/>
               <img src='./assets/bulb3.jpg' alt='img-demo' className='w-[100%] h-[90%] rounded-md '/>
               <img src='./assets/deathtree.jpg' alt='img-demo' className='w-[100%] h-[90%] rounded-md '/>
-              <img src='./assets/road.jpg' alt='img-demo' className='w-[100%] h-[90%] rounded-md '/>
+              <img src='./assets/juice.jpg' alt='img-demo' className='w-[100%] h-[90%] rounded-md '/>
             </div>
           </div>
     </div>
